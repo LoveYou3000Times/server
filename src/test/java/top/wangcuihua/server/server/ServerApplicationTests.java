e9555b3104262d250fbc7ecd88e27d4d082f80d3
WangFuGui <1123678232@qq.com>
2020-01-18 00:58:53 +0800

    提交demo

---
+++ src/test/java/top/wangcuihua/server/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.wangcuihua.server.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
